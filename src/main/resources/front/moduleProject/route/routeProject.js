f8c9c194c76b6fea209fbcb1efb4216afabad7ff
Julie TAN <jtan@DIGINAMIC.LOCAL>
2018-01-19 17:46:47 +0100

    front: route for home

--- src/main/resources/front/moduleProject/route/routeProject.js
+++ src/main/resources/front/moduleProject/route/routeProject.js
@@ -1,7 +1,8 @@
 angular.module('project').config(['$routeProvider', function($routeProvider) {
     $routeProvider
     	.when('/home', {
-    		templateUrl: "./html/home.html"
+    		templateUrl: './html/home.html',
+    		controller: 'homeCtrl'
     	})
         .when('/research', {
             templateUrl: './templates/research.html',
