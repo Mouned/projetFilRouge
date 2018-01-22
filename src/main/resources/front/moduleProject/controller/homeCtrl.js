04504dc6bc58097f6d7db90e378c602d2299ec33
Samuel JUZAN <sjuzan@DIGINAMIC.LOCAL>
2018-01-22 14:24:29 +0100

    front: home page - carroussel

--- src/main/resources/front/moduleProject/controller/homeCtrl.js
+++ src/main/resources/front/moduleProject/controller/homeCtrl.js
@@ -1,6 +1,9 @@
 /**
  * Controleur pour la page home
  */
-angular.module('project').controller('homeCtrl', ['$scope', function($scope) {
-	
+angular.module('project').controller('homeCtrl', ['$scope', 'gameService', function($scope, gameService) {
+	console.log('controller for home');
+	gameService.getAll().then(function(data){
+		$scope.list = data;
+	});
 }]);
