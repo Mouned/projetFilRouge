95db9436c81147fb3adc5eded0b1726e3b015c8a
Xalkinn <julien.tisserand@outlook.com>
2018-01-22 14:26:21 +0100

    Add controller for gamelist

--- src/main/resources/front/moduleProject/controller/controllerProject.js
+++ src/main/resources/front/moduleProject/controller/controllerProject.js
@@ -1,4 +1,18 @@
 angular.module('project').controller('controllerProject', function ($scope, $http) {
+    $scope.liste = [];
+    $http.get('http://192.168.1.13:8080/api/public/products').then(function (response) {
+        console.log('RechercheMedia', response);
+        for(var key in response.products) {
+            var jeux = response.products[key];
+            $scope.liste.push(jeux);
+        }
+    });/*
+    var addJeux = function(Jeux){
+        return $http.post('url : ajout dun jeux', Jeux);
+    };
 
-
+    $scope.ajout = function(Jeux){
+        console.log(Jeux);
+        addJeux(Jeux).then();
+    }*/
 });
