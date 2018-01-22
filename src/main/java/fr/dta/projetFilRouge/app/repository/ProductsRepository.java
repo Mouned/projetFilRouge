5fff2f3814d4e2d48dcf106c7fd78adcb5a27f27
Julie TAN <jtan@DIGINAMIC.LOCAL>
2018-01-22 14:17:40 +0100

    products controller

--- src/main/java/fr/dta/projetFilRouge/app/repository/ProductsRepository.java
+++ src/main/java/fr/dta/projetFilRouge/app/repository/ProductsRepository.java
@@ -7,10 +7,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.dta.projetFilRouge.user.entity.Products;
 
+
+@Transactional
 public interface ProductsRepository extends JpaRepository<Products, Long>,PagingAndSortingRepository<Products, Long>{
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// RECHERCHER produit
+
 
 	List<Products> findByTitle(@Param("title") String title);
 	
@@ -19,4 +25,8 @@ public interface ProductsRepository extends JpaRepository<Products, Long>,Paging
 	List<Products> findAll(); 
 	
 	Page<Products> findAll(Pageable pageable);
+	
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// AJOUTER produit
+	
 }
