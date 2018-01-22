bde1eac18d7e452855d0d83803ce84fcfcc9334b
Samuel JUZAN <sjuzan@DIGINAMIC.LOCAL>
2018-01-22 14:43:54 +0100

    zzzzzzzz

--- src/main/resources/front/moduleProject/service/gameService.js
+++ src/main/resources/front/moduleProject/service/gameService.js
@@ -3,10 +3,10 @@
  */
 angular.module('project').service('gameService', ['$http', function($http){
 	
-	var host = 'http://192.168.1.13';
-	var port = '8080';
-	var p = 0;
-	var mediaPromise= $http.get('http://192.168.1.13:8080/api/public/products/all');
+//	var host = 'http://192.168.1.13';
+//	var port = '8080';
+//	var p = 0;
+	var mediaPromise= $http.get('localhost:8080/api/public/products/all');
 	
 //	getting the entire game list
 	this.getAll = function() {
