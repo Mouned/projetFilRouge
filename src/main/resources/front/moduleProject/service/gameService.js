b95da6ae47753a2bb31a3e452821966e18660fd5
Julie TAN <jtan@DIGINAMIC.LOCAL>
2018-01-23 10:56:39 +0100

    test

--- src/main/resources/front/moduleProject/service/gameService.js
+++ src/main/resources/front/moduleProject/service/gameService.js
@@ -14,12 +14,8 @@ angular.module('project').service('gameService', ['$http', function($http){
 	
 //	getting the entire game list
 	this.getAll = function() {
-<<<<<<< HEAD
 		return $http.get('http://192.168.1.17:8080/api/products/all').then(function(response) {
 			console.log(response);
-=======
-		return $http.get('http://192.168.1.25:8080/api/products/all').then(function(response) {
->>>>>>> 1e3fdb5b11dc64bb3c3c96c8155032c53180bdb4
 				return response;
 			});
 	}
