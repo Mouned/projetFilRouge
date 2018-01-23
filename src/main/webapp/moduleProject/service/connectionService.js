73d8fe98252321f6b760a8f0ad32962f6b1efdba
mouned <moussa.nedjari92@gmail.com>
2018-01-23 15:01:31 +0100

    [TECH] Déplacement des données de FRONT vers WEBAPP.

--- src/main/webapp/moduleProject/service/connectionService.js
+++ src/main/webapp/moduleProject/service/connectionService.js
@@ -2,8 +2,8 @@
  * Service for connection
  */
 angular.module('project').service('connectionService', ['$http', function($http) {
-	this.getUser = function(login, password) {
-		return $http.get('http://192.168.1.17:8080/authenticate?username='+login+'&password='+password)
+	this.getUser = function(username,password) {
+		return $http.post('/authenticate', {username : username, password:password})
 			.then(function(response){
 				console.log(response);
 				return response.data;
