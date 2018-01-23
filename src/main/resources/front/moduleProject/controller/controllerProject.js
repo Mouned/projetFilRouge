1e3fdb5b11dc64bb3c3c96c8155032c53180bdb4
Xalkinn <julien.tisserand@outlook.com>
2018-01-23 10:43:55 +0100

    Add new server

--- src/main/resources/front/moduleProject/controller/controllerProject.js
+++ src/main/resources/front/moduleProject/controller/controllerProject.js
@@ -1,6 +1,6 @@
 angular.module('project').controller('controllerProject', function ($scope, $http) {
     $scope.liste = [];
-    $http.get('http://192.168.1.28:8080/api/public/products/all').then(function (response) {
+    $http.get('http://192.168.1.17:8080/api/public/products/all').then(function (response) {
         console.log('RechercheMedia', response);
         for(var key in response.data) {
             var jeux = response.data[key];
