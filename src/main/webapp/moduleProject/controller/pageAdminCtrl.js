97346f9e4282f47dca034f6890b5c8882b5c4749
Samuel JUZAN <sjuzan@DIGINAMIC.LOCAL>
2018-01-25 10:49:53 +0100

    aaaaaaa

--- src/main/webapp/moduleProject/controller/pageAdminCtrl.js
+++ src/main/webapp/moduleProject/controller/pageAdminCtrl.js
@@ -38,6 +38,51 @@ angular.module('project').controller('pageAdminCtrl', ['connectionService', '$sc
 	    });
 	};
 	
+	//MODAL EDIT
+	$scope.openModalEdit = function(){
+		
+		// Controller + Template de la modale
+		var modalInstance = $uibModal.open({
+			templateUrl: './html/formAddProduct.html',
+			controller : function($scope,productsService){
+	        	$scope.addGame = function(jeu, file){
+	        		$scope.$close({jeu : angular.copy($scope.jeu), file : $scope.file});
+	        	};
+	        	$scope.pegis = [];
+	        	$scope.types = ["Survie", "Stratégie", "Action", "FPS", "Course","Monde ouvert","Découverte","Aventure"];
+	        	productsService.getPegis().then(function(response) {
+	        		$scope.pegis = response;
+	        	});
+			}
+	    });
+
+	    modalInstance.result.then(function (response) {
+			productsService.addGame(response.jeu,response.file).then(function(data){
+				$scope.liste = data;
+				console.log($scope.liste);
+			});
+	    }, function () {
+	    });
+	};
+	
+	//MODAL DETAIL
+	$scope.openModalDetail = function(){
+		var modalInstance = $uibModal.open({
+			templateUrl: './html/detailProduct.html',
+			controller : function($scope,productsService){
+	        	
+			}
+	    });
+
+	    modalInstance.result.then(function (response) {
+			productsService.addGame(response.jeu,response.file).then(function(data){
+				$scope.liste = data;
+				console.log($scope.liste);
+			});
+	    }, function () {
+	    });
+	}
+	
 	/**
 	 * To know if the user is admin
 	 */
