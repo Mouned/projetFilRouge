5009c408df3c3203afc9ba67d62bb6ff98264b5b
Samuel JUZAN <sjuzan@DIGINAMIC.LOCAL>
2018-01-25 12:51:43 +0100

    ddddd

--- src/main/webapp/moduleProject/controller/pageAdminCtrl.js
+++ src/main/webapp/moduleProject/controller/pageAdminCtrl.js
@@ -39,15 +39,19 @@ angular.module('project').controller('pageAdminCtrl', ['connectionService', '$sc
 	};
 	
 	//MODAL EDIT
-	$scope.openModalEdit = function(){
+	$scope.openModalEdit = function(jeuToEdit){
 		
 		// Controller + Template de la modale
 		var modalInstance = $uibModal.open({
-			templateUrl: './html/formAddProduct.html',
-			controller : function($scope,productsService){
+			templateUrl: './html/editProduct.html',
+			resolve : {
+				item : jeuToEdit
+			},
+			controller : function($scope, item, productsService){
 	        	$scope.addGame = function(jeu, file){
 	        		$scope.$close({jeu : angular.copy($scope.jeu), file : $scope.file});
 	        	};
+	        	$scope.jeu = item;
 	        	$scope.pegis = [];
 	        	$scope.types = ["Survie", "Stratégie", "Action", "FPS", "Course","Monde ouvert","Découverte","Aventure"];
 	        	productsService.getPegis().then(function(response) {
@@ -66,21 +70,27 @@ angular.module('project').controller('pageAdminCtrl', ['connectionService', '$sc
 	};
 	
 	//MODAL DETAIL
-	$scope.openModalDetail = function(){
+	$scope.openModalDetail = function(jeuToEdit){
 		var modalInstance = $uibModal.open({
 			templateUrl: './html/detailProduct.html',
-			controller : function($scope,productsService){
-	        	
+			resolve : {
+				item : jeuToEdit
+			},
+			controller : function($scope, item){
+				$scope.addGame = function(jeu, file){
+	        		$scope.$close({jeu : angular.copy($scope.jeu), file : $scope.file});
+	        	};
+				$scope.jeu = item;
 			}
 	    });
 
-	    modalInstance.result.then(function (response) {
-			productsService.addGame(response.jeu,response.file).then(function(data){
-				$scope.liste = data;
-				console.log($scope.liste);
-			});
-	    }, function () {
-	    });
+//	    modalInstance.result.then(function (response) {
+//			productsService.addGame(response.jeu,response.file).then(function(data){
+//				$scope.liste = data;
+//				console.log($scope.liste);
+//			});
+//	    }, function () {
+//	    });
 	}
 	
 	/**
