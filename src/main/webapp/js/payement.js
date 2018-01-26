09cf3ccd2cd599d6c652a225e47e610fb6aefdf9
Samuel JUZAN <sjuzan@DIGINAMIC.LOCAL>
2018-01-26 16:47:07 +0100

    aaa

---
+++ src/main/webapp/js/payement.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+    $("#back, #front").click(function () {
+        if (this.id == "back") {
+            $(".flip-container").addClass("flip");
+        }
+        else if (this.id == "front") {
+            $(".flip-container").removeClass("flip");
+        }
+    });
+    $("#name, #number, #month, #cvv, #year").bind("change paste keyup", function() {
+        if (this.id == "name") {
+            $(".card__name").text($(this).val());
+        }
+        else if (this.id == "number") {
+            var VISA_PREFIXES = ["4539", "4556", "4916", "4532", "4929", "4485", "4716"]; // Prefixes for VISA cards
+            var MC_PREFIXES = ["51", "52", "53", "54", "55"]; // Prefixes for Master Card cards
+            var field_value = $(this).val();
+
+            if (field_value.charAt(0) === "4" && field_value.length >= 4) {
+                var prefix = field_value.slice(0, 4);
+
+                if (VISA_PREFIXES.includes(prefix)) {
+                    $("#card__image").attr("src", "https://k60.kn3.net/F/7/8/1/A/3/D23.png");
+                }
+            } else if (field_value.charAt(0) === "5" && field_value.length >= 2) {
+                var prefix = field_value.slice(0, 2);
+
+                if (MC_PREFIXES.includes(prefix)) {
+                    $("#card__image").attr("src", "https://k60.kn3.net/8/C/F/A/8/9/B0E.png");
+                }
+            } else {
+                $("#card__image").attr("src", "");
+            }
+
+            $(".card__number").text(field_value);
+        }
+        else if (this.id == "month") {
+            $(".card__month").text($(this).val());
+        }
+        else if (this.id == "cvv") {
+            $(".card__cvv").text($(this).val());
+        }
+        else if (this.id == "year") {
+            $(".card__year").text($(this).val());
+        }
+    });
+});
