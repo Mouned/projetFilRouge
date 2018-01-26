31cc60766b4fb927955e0e63b4fe80eee23f8e48
Julie TAN <jtan@DIGINAMIC.LOCAL>
2018-01-26 15:06:17 +0100

    front: edit profile with empty input for password

--- src/main/webapp/moduleProject/controller/pageUserCtrl.js
+++ src/main/webapp/moduleProject/controller/pageUserCtrl.js
@@ -3,8 +3,8 @@
  */
 angular.module('project').controller('pageUserCtrl', ['$scope', '$routeParams', 'pageUserService', 'connectionService', function($scope, $routeParams, pageUserService, connectionService) {
 	$scope.user = connectionService.getUser();
-	$scope.edit = function(user) {
-		pageUserService.editUser(user).then(function(){
+	$scope.edit = function(user, pass) {
+		pageUserService.editUser(user, pass).then(function(){
 			console.log('profil modifié !');
 		});
 	}
--- src/main/webapp/moduleProject/service/pageUserService.js
+++ src/main/webapp/moduleProject/service/pageUserService.js
@@ -3,7 +3,8 @@
  */
 angular.module('project').service('pageUserService', ['$http', function($http) {
 	
-	this.editUser = function(user) {
+	this.editUser = function(user, pass) {
+		user.password = pass;
 		return $http.post('/api/users/update', user).then(function(response) {
 			return response.data;
 		})
