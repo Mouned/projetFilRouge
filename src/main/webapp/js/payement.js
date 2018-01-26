d70bc269facbcb06fb9c417b4e40665e6f860835
Xalkinn <julien.tisserand@outlook.com>
2018-01-26 17:54:57 +0100

    Add credit cart

--- src/main/webapp/js/payement.js
+++ src/main/webapp/js/payement.js
@@ -1,47 +1,70 @@
-$( document ).ready(function() {
-    $("#back, #front").click(function () {
-        if (this.id == "back") {
-            $(".flip-container").addClass("flip");
-        }
-        else if (this.id == "front") {
-            $(".flip-container").removeClass("flip");
-        }
-    });
-    $("#name, #number, #month, #cvv, #year").bind("change paste keyup", function() {
-        if (this.id == "name") {
-            $(".card__name").text($(this).val());
-        }
-        else if (this.id == "number") {
-            var VISA_PREFIXES = ["4539", "4556", "4916", "4532", "4929", "4485", "4716"]; // Prefixes for VISA cards
-            var MC_PREFIXES = ["51", "52", "53", "54", "55"]; // Prefixes for Master Card cards
-            var field_value = $(this).val();
+$('.input-cart-number').on('keyup change', function(){
+    $t = $(this);
 
-            if (field_value.charAt(0) === "4" && field_value.length >= 4) {
-                var prefix = field_value.slice(0, 4);
+    if ($t.val().length > 3) {
+        $t.next().focus();
+    }
 
-                if (VISA_PREFIXES.includes(prefix)) {
-                    $("#card__image").attr("src", "https://k60.kn3.net/F/7/8/1/A/3/D23.png");
-                }
-            } else if (field_value.charAt(0) === "5" && field_value.length >= 2) {
-                var prefix = field_value.slice(0, 2);
+    var card_number = '';
+    $('.input-cart-number').each(function(){
+        card_number += $(this).val() + ' ';
+        if ($(this).val().length == 4) {
+            $(this).next().focus();
+        }
+    })
 
-                if (MC_PREFIXES.includes(prefix)) {
-                    $("#card__image").attr("src", "https://k60.kn3.net/8/C/F/A/8/9/B0E.png");
-                }
-            } else {
-                $("#card__image").attr("src", "");
-            }
-
-            $(".card__number").text(field_value);
-        }
-        else if (this.id == "month") {
-            $(".card__month").text($(this).val());
-        }
-        else if (this.id == "cvv") {
-            $(".card__cvv").text($(this).val());
-        }
-        else if (this.id == "year") {
-            $(".card__year").text($(this).val());
-        }
-    });
+    $('.credit-card-box .number').html(card_number);
 });
+
+$('#card-holder').on('keyup change', function(){
+    $t = $(this);
+    $('.credit-card-box .card-holder div').html($t.val());
+});
+
+$('#card-holder').on('keyup change', function(){
+    $t = $(this);
+    $('.credit-card-box .card-holder div').html($t.val());
+});
+
+$('#card-expiration-month, #card-expiration-year').change(function(){
+    m = $('#card-expiration-month option').index($('#card-expiration-month option:selected'));
+    m = (m < 10) ? '0' + m : m;
+    y = $('#card-expiration-year').val().substr(2,2);
+    $('.card-expiration-date div').html(m + '/' + y);
+})
+
+$('#card-ccv').on('focus', function(){
+    $('.credit-card-box').addClass('hover');
+}).on('blur', function(){
+    $('.credit-card-box').removeClass('hover');
+}).on('keyup change', function(){
+    $('.ccv div').html($(this).val());
+});
+
+
+/*--------------------
+CodePen Tile Preview
+--------------------*/
+
+setTimeout(function(){
+    $('#card-ccv').focus().delay(1000).queue(function(){
+        $(this).blur().dequeue();
+    });
+}, 500);
+
+/*function getCreditCardType(accountNumber) {
+  if (/^5[1-5]/.test(accountNumber)) {
+    result = 'mastercard';
+  } else if (/^4/.test(accountNumber)) {
+    result = 'visa';
+  } else if ( /^(5018|5020|5038|6304|6759|676[1-3])/.test(accountNumber)) {
+    result = 'maestro';
+  } else {
+    result = 'unknown'
+  }
+  return result;
+}
+
+$('#card-number').change(function(){
+  console.log(getCreditCardType($(this).val()));
+})*/
